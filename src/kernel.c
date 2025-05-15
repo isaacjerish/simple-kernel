388af8b14a3f97dcaf73af5a2eae5bdc3b96735b
Isaac Jerish <152412476+isaacjerish@users.noreply.github.com>
2025-05-14 22:35:17 -0400

    Screen clear with black background and light red text using direct pointer to video memory in protected mode, then writing each character (with format) byte by byte using while loop

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,14 @@
 void kmain(void) {
-    volatile char* video_memory = (volatile char*)0xb8000;
-    video_memory[0] = 'T';
-    video_memory[1] = 0x0F; 
+    char* vid_memory  = (char*) 0xB8000;
+    char* string = "Hello World from Isaac's Kernel!";
+    unsigned short i, j, k;
+    i = 0;
+    j = 0;
+    k = 80 * 25 *2;
+    while (i < k) {
+        vid_memory[i]= ' ';
+        vid_memory[++i] = 0x0C;
+        i++;
+    }
+
 }
