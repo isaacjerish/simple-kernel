d39a89f5141732a5bd321a6fd9ec0543e481b966
Isaac Jerish <152412476+isaacjerish@users.noreply.github.com>
2025-05-14 23:04:43 -0400

    Implemented read and write port functions in assembly

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,20 +1,31 @@
-       bits 32
-       section .text
-       
-       align 4
-       dd 0x1BADB002 ;the magic number for multiboot spec header
-       dd 0x00 ; the flags for multiboost spec header
-       dd - (0x1BADB002 + 0x00) ;checksum 
-       
-       global start
-       extern kmain
-       
+            bits 32
+            section .text
+            
+            align 4
+            dd 0x1BADB002 ;the magic number for multiboot spec header
+            dd 0x00 ; the flags for multiboost spec header
+            dd - (0x1BADB002 + 0x00) ;checksum 
+            
+            global start
+            global read_port
+            global write_port
+            extern kmain
+            
 start:  
-       cli
-       mov esp, stack
-       call kmain
-       hlt
-       
-       section .bss
-       resb 8000
+            cli
+            mov esp, stack
+            call kmain
+            hlt
+read_port:  
+            mov edx, [esp + 4]
+            in al, dx
+            ret
+write_port:  
+            mov edx, [esp +4]
+            mov al, [esp + 8]
+            out dx, al
+            ret
+            
+            section .bss
+            resb 8000
 stack:  
